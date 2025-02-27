7991af640056b77f5f4dca2bd4752c9940212a10
Jonas Lefdal <jonas.lefdal@gmail.com>
2025-02-27 17:41:48 +0100

    Depolyment-test

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
-
-const withPWA = require('next-pwa')({
-	dest: 'public',
+const withPWA = require("next-pwa")({
+	dest: "public",
 	register: true,
 	skipWaiting: true,
-})
-
-module.exports = withPWA({
+  });
+  
+  const nextConfig = withPWA({
+	output: "export", // Enables static export
+	images: {
+	  unoptimized: true, // Required for GitHub Pages
+	},
+	basePath: "/DartApp", // Change this to match your GitHub repo name
+	assetPrefix: "/DartApp/",
 	reactStrictMode: true,
-})
+  });
+  
+  module.exports = nextConfig;
+  
