097032380c44f58af438fe7db45cddeed9a9d190
Jonas Lefdal <jonas.lefdal@gmail.com>
2025-02-27 18:13:46 +0100

    fixes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,14 +10,16 @@ export default function Document() {
 					name='viewport'
 					content='width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover'
 				/>
-				<meta
-					name='theme-color'
-					content='#18181b'
-					media='(prefers-color-scheme: dark)'
-				/>
-				<meta name='theme-color' content='#f4f4f5' />
+
+				<meta name="theme-color" content="#18181b" media="(prefers-color-scheme: dark)" />
+				<meta name="theme-color" content="#f4f4f5" media="(prefers-color-scheme: light)" />
+				
 				<link rel='apple-touch-icon' href='/images/icon-maskable-512.png' />
 				<link rel='manifest' href='/manifest.json' />
+				
+				<meta name="apple-mobile-web-app-capable" content="yes" />
+				<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+				<meta name="apple-mobile-web-app-title" content="DartApp" />
 			</Head>
 			<body>
 				<Main />
