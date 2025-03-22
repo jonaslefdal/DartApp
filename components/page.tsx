1a148be90f96dc36d30b9e11803905e683570d5e
Jonas Lefdal <jonas.lefdal@gmail.com>
2025-03-22 03:09:28 +0100

    scroll

--- components/page.tsx
+++ components/page.tsx
@@ -22,7 +22,7 @@ const Page = ({ title, children }: Props) => (
 			 * Padding top = `appbar` height
 			 * Padding bottom = `bottom-nav` height
 			 */
-			className='mx-auto max-w-screen-md pb-24 pt-20 px-safe'
+			className='mx-auto max-w-screen-md pt-20 pb-24 px-safe sm:pb-0'
 		>
 			<div className='p-6'>{children}</div>
 		</main>
